7eda2b52ca8f9f7b8517af723c050a59fcd49a71
fabrizziohc15 <fabrizziohc15@gmail.com>
2023-12-05 09:08:06 -0600

    definicion de pruebas

---
+++ revision.py
@@ -0,0 +1,43 @@
+# revision.py
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def revisar_llegada():
+    # TODO: Agregar pruebas para la función llegada
+    pass
+
+def revisar_servicio():
+    # TODO: Agregar pruebas para la función servicio
+    pass
+
+def revisar_parametros():
+    # TODO: Agregar pruebas para la función parametros
+    pass
+
+def revisar_sistema():
+    # TODO: Agregar pruebas para la función sistema
+    pass
+
+def revisar_visualizacion():
+    # TODO: Agregar pruebas para la función visualizacion
+    pass
+
+def revisar_probabilidad():
+    # TODO: Agregar pruebas para la función probabilidad
+    pass
+
+def revisar_fila():
+    # TODO: Agregar pruebas para la función fila
+    pass
+
+def revisar_servidores():
+    # TODO: Agregar pruebas para la función servidores
+    pass
+
+def revisar_tiempo():
+    # TODO: Agregar pruebas para la función tiempo
+    pass
+
+# Agregar cualquier observación adicional o comentarios necesarios
